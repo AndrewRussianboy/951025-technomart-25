317f744c51092608b2b34f3548b6aa1e0d52ac56
AndrewRussianboy <andrewdruzhaev@gmail.com>
2019-07-05 16:26:42 +0300

    Project is finished
    Жду комментарии)

---
+++ js/script.js
@@ -0,0 +1,60 @@
+var link = document.querySelector(".button-write");
+
+var popup = document.querySelector(".modal-review");
+var close = popup.querySelector(".modal-close");
+
+var yourname = popup.querySelector("[name=yourname]");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    yourname.focus();
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
+    }
+});
+
+
+
+var mapLink = document.querySelector(".open-map");
+
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
+
+
+
+var cartLink = document.querySelector(".buy");
+
+var cartPopup = document.querySelector(".item-added");
+var cartClose = cartPopup.querySelector(".modal-close");
+
+cartLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal-show");
+});
+
+cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+});
